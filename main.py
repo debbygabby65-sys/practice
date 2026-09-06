86f4e54134e84ed85e4cf09b5f941390dd3aa5d2
debbygabby65-sys <debbygabby65@gmail.com>
2026-09-06 20:59:49 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,74 @@
+text = 'python programming'
+print(text[::-1])
+print(text[0:6])
+print(text[0:6:4])
+
+text = 'Debby'
+print(text[::-1])
+
+text = 'Debby'
+print(text[0:5:2])
+
+language = 'Python'
+print(language[-1])
+print(language[-2])
+print(language[0:-5])
+
+text = 'APPLICATION'
+print(text[0])
+print(text[-7])
+print(text[-2])
+print(text[-1])
+
+
+name = 'Debby'
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+
+word = 'python'
+print(word[0])
+print(word[-1])
+
+name = 'Debby'
+print(name[0:6:4])
+
+email = 'debby@gmail.com'
+print(email[0:5])
+print(email[6:])
+
+email = 'john@gmail.com'
+print(email[0:4])
+print(email[5:])
+
+sentence = 'I love python programming'
+print(sentence[0:6])
+print(sentence[7:13])
+print(sentence[14:])
+
+word = 'Hello World'
+print(word[0:5])
+print(word[6:])
+
+
+word = 'Python'
+print(word[-1])
+print(word[-2])
+print(word[0:6])
+
+name = '            Debby        '
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+print(name.upper().strip())
+print(name.lower().strip())
+print(name.capitalize().strip())
+
+email = '               DEBBY@GMAIL.COM         '
+print(email.lower().strip())
+
+phone = "070-7093-2327"
+print(phone.replace("-", " "))
