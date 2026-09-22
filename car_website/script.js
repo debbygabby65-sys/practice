d7276c295e42236b10262da70cb2624b2e5a4f22
debbygabby65-sys <debbygabby65@gmail.com>
2026-09-22 15:37:32 +0100

    initial car_website

---
+++ car_website/script.js
@@ -0,0 +1,15 @@
+const brand = document.getElementById ("brand");
+const model = document.getElementById("model");
+const year = document.getElementById("year");
+const registerButton = document.getElementById("registerButton")
+registerButton.addEventListener("click", function (event) {event.preventDefault()
+console.log("Car registration started");
+const currentBrand = brand.value
+console.log(currentBrand)
+const currentModel = model.value
+console.log(currentModel)
+const currentYear = year.value
+console.log(currentYear)});
+
+registerButton.addEventListener(const carForm = document.getElementById("carForm")
+
