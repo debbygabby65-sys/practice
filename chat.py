3634a2d8ecbcb244fc179ecbcb689297982ae4b8
debbygabby65-sys <debbygabby65@gmail.com>
2026-09-25 16:55:03 +0100

    chat.py

---
+++ chat.py
@@ -0,0 +1,10 @@
+cars = ["Toyota", "Honda", "BMW" ]
+print(cars[1])
+cars[2] = "Mercedes"
+print(cars)
+cars.append("Kia")
+print(cars)
+cars.extend(["Ford", "Audi"])
+print(cars)
+cars.remove("Mercedes")
+print(cars)
