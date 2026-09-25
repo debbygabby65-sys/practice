dec26e805477248a2a73b0d51cd67270d9f65c12
debbygabby65-sys <debbygabby65@gmail.com>
2026-09-25 10:29:52 +0100

    command.py

--- command.py
+++ command.py
@@ -1,1 +1,20 @@
-import raph
+profile = ["Debby", "Lagos", 25]
+profile_copy = profile.copy()
+profile_copy[0] = "Gabby"
+print(profile)
+print(profile_copy)
+
+profile = ["Debby", "Lagos", 25]
+profile_copy = profile.copy()
+profile_copy[0] = "Gabby"
+profile_copy[2] = 30
+print(profile)
+print(profile_copy)
+
+
+
+
+def add_numbers(a, b):
+    return a + b
+result = add_numbers(50, 10)
+print(result)
